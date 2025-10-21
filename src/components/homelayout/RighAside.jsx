3979ca58039d59357b8d72834f60dd6894622c81
Himel <shahriarhasan221@gmail.com>
2025-10-21 12:13:41 +0600

    implemented sticky on both sides

--- src/components/homelayout/RighAside.jsx
+++ src/components/homelayout/RighAside.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import SocialLogin from "./SocialLogin";
 import FindUs from "./FindUs";
-import QZone from "./QZone";
+import QZone from "./Qzone";
 
 const RighAside = () => {
   return (
